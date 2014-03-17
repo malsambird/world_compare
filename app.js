7b75f23a8faa09cc9f126726c62657ab7551015f
Malcolm Bird <bird.malcolm@gmail.com>
2014-03-17 09:45:15 +0900

    added api

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 $(document).ready( function() {
 
 
-
+console.log("startered");
 
 var happytest = $.ajax({
-	url: "https://api.instagram.com/v1/tags/100happydays/media/recent?access_token=ACCESS-TOKEN",
+	url: "https://api.instagram.com/v1/tags/100happydays/media/recent?client_id=b8450f13c6f04d21b2d85661cc8a1923",
+	data: {"filter": 'Willow'},
 	dataType: "jsonp",
 	type: "GET",
 	})
 	.done(function(happytest){
 		console.debug("11");
 		console.log(happytest);
-		$.each(function(i, item){
+		$.each(happytest.data, function(i, item){
 			console.debug("13");
+			console.log(i);
 			var showPic = function(){
 				var div = $('.container');
-				var Picture = item.data.images.lowresolution;
-				div.append(Picture);
+				var Picture = item.images.low_resolution.url;
+				div.append("<img src =" + Picture +">");
+				console.log("got to 21");
 			};
 			showPic(item);
 		});
@@ -24,8 +27,10 @@ var happytest = $.ajax({
 		
 	});
 
+//"https://api.instagram.com/v1/tags/100happydays/media/recent?access_token=ACCESS-TOKEN",
+var client_id = "b8450f13c6f04d21b2d85661cc8a1923";
 
-
+//google API AIzaSyDPb7OwBHpTcv5N_rci1FcQZZiOPvrIZjQ
 
 
 
