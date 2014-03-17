669c1182c3a88b39c207d1f440136b6f40811a92
Malcolm Bird <bird.malcolm@gmail.com>
2014-03-17 10:28:23 +0900

    ff

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 $(document).ready( function() {
-
+var API_KEY = "AIzaSyDQFaTMrBAaVCQcWr6e1suw9kSYw44cq40"; //googlemaps
 
 console.log("startered");
+function initialize() {
+        var mapOptions = {
+          center: new google.maps.LatLng(-34.397, 150.644),
+          zoom: 8
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"),
+            mapOptions);
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+
+
 
 var happytest = $.ajax({
 	url: "https://api.instagram.com/v1/tags/100happydays/media/recent?client_id=b8450f13c6f04d21b2d85661cc8a1923",
@@ -29,7 +40,7 @@ var happytest = $.ajax({
 
 //"https://api.instagram.com/v1/tags/100happydays/media/recent?access_token=ACCESS-TOKEN",
 var client_id = "b8450f13c6f04d21b2d85661cc8a1923";
-//new google api key AIzaSyDQFaTMrBAaVCQcWr6e1suw9kSYw44cq40
+
 //google API AIzaSyDPb7OwBHpTcv5N_rci1FcQZZiOPvrIZjQ
 
 
