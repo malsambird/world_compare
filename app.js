8033a0c0fd0216c5e689f6d96a87eefd38850131
Malcolm Bird <bird.malcolm@gmail.com>
2014-03-18 22:08:26 +0900

    functional

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ $(document).ready( function() {
 var client_id = "b8450f13c6f04d21b2d85661cc8a1923";
 
 //This prototype is not being used yet. 
-function Star(longitude, latitude, url) {
-	this.longitude = longitude;
-	this.latitude = latitude;
-	this.url = url;
-}
+//function Star(longitude, latitude, url) {
+//	this.longitude = longitude;
+//	this.latitude = latitude;
+//	this.url = url;
+//}
 // end of prototype
 
 
@@ -18,11 +18,12 @@ function Star(longitude, latitude, url) {
 var latitude = 0; // (φ)
 var longitude = 0;   // (λ)
 var newPicURL;
+var caption;
 var x;
 var y;
 var div = $('.container');
-var mapWidth    = 800;
-var mapHeight   = 800;
+var mapWidth    = 550;
+var mapHeight   = 550;
 
 
 // Creating a new star on the map corresponding with pictures. 
@@ -43,10 +44,11 @@ var createNewStar = function (){
 	//Clone Star image and place it in the correct location
 	var newStar = $("#marker")
 	.clone()
-	.css('top', y -32+ 'px')
-	.css('left', x  + 'px')
+	.css('top', y -16+ 'px')
+	.css('left', x  -16 + 'px')
 	.css('position', 'absolute')
 	.attr('data', newPicURL)
+	.attr('data-caption', caption + " ")
 	.show();
 	console.log(newPicURL);
 	console.log("new star created");
@@ -72,18 +74,15 @@ var happytest = $.ajax({
 			} else {
 				//if they have location coordinates, put star on map
 				console.debug("else");
-				i = new Star(+item.location.longitude, +item.location.latitude, item.images.low_resolution.url);
+				//i = new Star(+item.location.longitude, +item.location.latitude, item.images.low_resolution.url);
 				latitude = +item.location.latitude;
 				longitude = +item.location.longitude;
 				newPicURL = item.images.low_resolution.url;
+				if (item.caption.text !== null){
+					caption = item.caption.text;
+					}
 				$("#mapcontainer").prepend(createNewStar);
-			
-				//shows the corresponding pictures. I eventually want to display the picture when the star is clicked. 
-				var showPic = function(){
-					var Picture = item.images.low_resolution.url;
-					div.append("<img src =" + Picture +">");
-					};
-				showPic(item);
+	
 			}
 		});
 	});
@@ -91,11 +90,16 @@ var happytest = $.ajax({
 $("div#mapcontainer").on('click', 'img.star', function(event){
 	event.preventDefault();
 	console.log("click");
-	console.log($(this));
-	var newPic = $(this).data();
+	console.log($(this).attr("data"));
+	var newPic = $(this).attr("data");
+	var newCaption = $(this).attr("data-caption");
 	console.log(newPic);
-	$("#current_pic").css('background-color', 'white');
+	$("#current_pic").css('background-image', 'url('+newPic+')');
+	$("#caption p").text(newCaption);
 	});
 
+$("button").click(function(){
+	happytest.done;
+});
 	
 });
